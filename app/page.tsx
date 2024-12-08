a7ef87d6cd921dec4359a60cd2964e2e0e0aaa48
bilalfareed83 <53287592+bilalfareed83@users.noreply.github.com>
2024-12-08 22:57:26 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 import BlogGrid from "@/components/blog";
-import Footer from "@/components/footer";
 import Display from "@/components/displat";
-import ProductListFeature from "@/components/feature";
 import ProductGrid from "@/components/feature";
 import RocketSingleSeater from "@/components/hero";
 import InstagramBanner from "@/components/insta";
-import Navbar from "@/components/nav";
 import ProductGridList from "@/components/product";
 
 export default function Home() {
